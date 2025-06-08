5dfa118cb6a7a61abadb3ac5e74000d63a2e458a
Aalok-kr <aalokstudy001@gmail.in>
2025-06-08 17:26:49 +0530

    Update addition.dart

--- Dart/addition.dart
+++ Dart/addition.dart
@@ -10,7 +10,7 @@ import 'dart:io';
 //   // Output: The sum of 5 and 10 is 15
 // }
 
-void main() {
+/*void main() {
   stdout.write("Enter first number: ");
   int num1 = int.parse(stdin.readLineSync()!);
 
@@ -21,4 +21,26 @@ void main() {
 
   print("The sum of $num1 + $num2");
   print(" is = $sum");
+}*/
+
+/* void main() {
+  stdout.write("Enter 1st no:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter 2nd no:");
+  int num2 = int.parse(stdin.readLineSync()!);
+  int sum = num1 * num2;
+  print("the sum of $num1 * $num2");
+  print("is = $sum");
+} */
+
+void main() {
+  stdout.write("enter 1st no:");
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write("enter 2nd no:");
+
+  int num2 = int.parse(stdin.readLineSync()!);
+  int div = num1 % num2;
+
+  print("the sum of $num1 % $num2");
+  print("is = $div");
 }
