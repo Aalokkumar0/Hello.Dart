fc4d4edd768a9792bbdaa50212a928a223ed4101
Aalok-kr <aalokstudy001@gmail.in>
2025-06-08 12:09:11 +0530

    Create addition.dart

---
+++ Dart/addition.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+// void main() {
+//   int num1 = 5;
+//   int num2 = 10;
+
+//   int sum = num1 + num2;
+//   print('The sum of $num1');
+//   print("and $num2 is = $sum");
+//   // Output: The sum of 5 and 10 is 15
+// }
+
+void main() {
+  stdout.write("Enter first number: ");
+  int num1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter second number: ");
+  int num2 = int.parse(stdin.readLineSync()!);
+
+  int sum = num1 + num2;
+
+  print("The sum of $num1 + $num2");
+  print(" is = $sum");
+}
