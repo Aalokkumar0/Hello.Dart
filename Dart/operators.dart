d4d94a315f0240d28dfade870f1680d5e5487084
Aalok-kr <aalokstudy001@gmail.in>
2025-06-10 11:58:06 +0530

    Update operators.dart

--- Dart/operators.dart
+++ Dart/operators.dart
@@ -33,9 +33,46 @@ void main() {
 }*/
 
 // useing the  ternary operator in Dart
-import 'dart:io';
+/*import 'dart:io';
 
 void main() {
-  stdout.write("Enter your value: ");
-  int number = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter your  first value: ");
+  int firstnumber = int.parse(stdin.readLineSync()!);
+  stdout.write("enter your second value:");
+  int secondnumber = int.parse(stdin.readLineSync()!);
+  int result = firstnumber > secondnumber ? firstnumber : secondnumber;
+  print("The greater number is: $result");
+}*/
+
+// use the string interpolation in Dart
+import 'dart:io';
+
+/* void main() {
+  stdout.write("enter your name:");
+  String name = stdin.readLineSync()!;
+  stdout.write("Enter your age:");
+  int age = int.parse(stdin.readLineSync()!);
+  stdout.write("Enter your hight:");
+  double height = double.parse(stdin.readLineSync()!);
+  String introduction =
+      "My name is $name, I am $age years old and my height is $height feet.";
+}*/
+
+// boolean operators in Dart
+
+void main() {
+  bool isTrue = true;
+  bool isFalse = false;
+
+  // Logical AND
+  bool andResult = isTrue && isFalse;
+  print('Logical AND: $andResult'); // Output: Logical AND: false
+
+  // Logical OR
+  bool orResult = isTrue || isFalse;
+  print('Logical OR: $orResult'); // Output: Logical OR: true
+
+  // Logical NOT
+  bool notResult = !isTrue;
+  print('Logical NOT: $notResult'); // Output: Logical NOT: false
 }
