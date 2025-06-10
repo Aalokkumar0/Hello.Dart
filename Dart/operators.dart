331982e70d9cadf7a90757031ca0d3114c860803
Aalok-kr <aalokstudy001@gmail.in>
2025-06-10 11:31:51 +0530

    datat

---
+++ Dart/operators.dart
@@ -0,0 +1,41 @@
+/*// use the arithmetic operators in Dart
+void main() {
+  int a = 10;
+  int b = 5;
+
+  // Addition
+  int sum = a + b;
+  print('Sum: $sum'); // Output: Sum: 15
+
+  // Subtraction
+  int difference = a - b;
+  print('Difference: $difference'); // Output: Difference: 5
+
+  // Multiplication
+  int product = a * b;
+  print('Product: $product'); // Output: Product: 50
+
+  // Division
+  double quotient = a / b;
+  print('Quotient: $quotient'); // Output: Quotient: 2.0
+
+  // Modulus
+  int remainder = a % b;
+  print('Remainder: $remainder'); // Output: Remainder: 0
+
+  // Increment
+  a++;
+  print('Incremented a: $a'); // Output: Incremented a: 11
+
+  // Decrement
+  b--;
+  print('Decremented b: $b'); // Output: Decremented b: 4
+}*/
+
+// useing the  ternary operator in Dart
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter your value: ");
+  int number = int.parse(stdin.readLineSync()!);
+}
---
+++ Dart/variable.dart
@@ -0,0 +1,13 @@
+// void main() {
+//   var aalok = 20;
+//   var sam = 30;
+
+//   var sum = aalok + sam;
+//   print("the sum of $aalok and $sam is = $sum");
+// }
+
+void main() {
+  var Aalok = 30, Sam = 40, sum;
+  sum = Aalok + Sam;
+  print("The sum of $Aalok and $Sam is = $sum");
+}
