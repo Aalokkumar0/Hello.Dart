c8a6d184271291bb4dab129752468ff02cb5abf2
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-09 16:50:49 +0530

    Add initial project configuration and example Dart files

---
+++ if_else.dart
@@ -0,0 +1,18 @@
+
+void main(){
+
+  int age = 62;
+
+
+  if( age < 10){
+    print("you are child :");
+  }
+  else if(age < 21 ){
+    print("you are teen ager bro do impresive");
+  }else if (age < 60){
+    print("how are you old main");
+  } else{
+    print("pls go back home");
+  }
+
+}
