7a49c5d9d24d709db939bcd8b7bf7fc6e5ced27c
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-10 11:03:04 +0530

    Refactor if_else.dart for improved formatting and readability; add for_loop.dart with initial implementation

---
+++ for_loop.dart
@@ -0,0 +1,9 @@
+void main(){
+  List <int> numbers = [1, 3, 5, 7, 9, 11, 34, 56, 78, 67];
+  print(numbers);
+
+print(0 < 9);
+
+for (int i = 9 ; i < 9 ; i++)
+print(numbers[i]);
+}
--- if_else.dart
+++ if_else.dart
@@ -1,22 +1,15 @@
-
-import 'dart:io';
-
-void main(){
-
+void main() {
   int age = 16;
 
-
-  if( age < 10){
+  if (age < 10) {
     print("you are child :");
-  }
-  else if(age < 21 ){
+  } else if (age < 21) {
     print("you are teen ager bro do impresive");
-  }else if (age < 60){
+  } else if (age < 60) {
     print("how are you old main");
-  } else{
+  } else {
     print("pls go back home");
   }
-
 
   String result = (age < 18) ? "you are not able to give vote" : "yes you are";
   print(result);
@@ -24,12 +17,26 @@ void main(){
   int num1 = 23;
   int num2 = 33;
 
-  int resultt = (num1 < num2) ? num1 : num2 ;
+  int resultt = (num1 < num2) ? num1 : num2;
   print(resultt);
 
-  print(num1>num2 ? "it is $num1":"its not$num2");
+  print(num1 > num2 ? "it is $num1" : "its not$num2");
 
   int A = 57;
-  print(A%2 == 0 ? "it is even":"it is odd");
-  
+  print(A % 2 == 0 ? "it is even" : "it is odd");
+
+  int marks = 100;
+  if (marks <= 30){
+        print("fail");
+  }else if (marks<= 50){
+    print("grade c");
+  }else if (marks<= 70){
+    print("grade b");
+  }else if(marks <= 90){
+    print("grade A");
+  }else if (marks <=100){
+    print("top");
+  }else{
+    print("pls Enter the valid marks");
   }
+}
