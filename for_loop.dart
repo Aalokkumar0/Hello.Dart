94f2e1bb531a745f231a3af4d8b3fefa6caf8bea
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-10 12:03:04 +0530

    Add switch statement example in swicth.dart and remove unused counter variable in main.dart

--- for_loop.dart
+++ for_loop.dart
@@ -1,9 +1,22 @@
+/*
+
+for(initialization; condition; increment){
+   code
+}
+
+*/
+
+
 void main(){
   List <int> numbers = [1, 3, 5, 7, 9, 11, 34, 56, 78, 67];
   print(numbers);
 
-print(0 < 9);
+  for (int i = 9 ; i >= 0 ; i--){
+    print(numbers[i]);
+  }
 
-for (int i = 9 ; i < 9 ; i++)
-print(numbers[i]);
+  for (int i = 0 ; i < 9+1 ; i++){
+    print(numbers[i]);
+  }
+
 }
---
+++ swicth.dart
@@ -0,0 +1,54 @@
+/*
+switch(variable){
+  case value1:
+     code
+     break;
+
+  case value2:
+     code
+     break;
+
+  default:
+     code
+} 
+
+*/
+
+
+void main(){
+  int day = 3;
+
+  switch(day){
+    case 1 :
+    print("monday");
+    break;
+
+    case 2:
+    print("tuesday");
+    break;
+
+    case 3:
+    print("wednesday");
+    break;
+
+    case 4:
+    print("thuresday");
+    break;
+
+    case 5:
+    print("friday");
+    break;
+
+    case 6:
+    print("seterday");
+    break;
+
+    case 7:
+    print("sunday");
+    break;
+
+    default:
+      print("invalid day");
+
+  }
+}
